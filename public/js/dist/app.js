240976f8035d7823dd2f25621d18d914ee11aaef
Daymond Blair <mondday@Daymonds-MacBook-Pro.local>
2020-06-22 15:21:53 -0700

    initialization

---
+++ public/js/dist/app.js
@@ -0,0 +1,20 @@
+// create 2 buttons
+// $(document.createElement('div').textContent('Button1'));
+// button 1
+// when clicked, console.log('name')
+
+// button 2
+// when hovered, counts from 1 to 25
+
+// square that turns red once mouse enters it
+
+$(function() {
+	
+});
+
+// create a div with class of box1 and a display of none as the style, with an h1 tag that says box1
+//create a button outside of the box1 that when clicked adds a class of active to box1. Now on your css file make the class active display block.
+
+//create a div with a class of box2 and add an h1 tag insdie of it that says box2. create an input text field then create a button under the input field.
+
+//Now when you click on this second button count how many letters inside of the input field then display that inside of the div with the class box2
